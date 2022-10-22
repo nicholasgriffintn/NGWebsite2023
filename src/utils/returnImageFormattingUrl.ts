a51f0a79c772857765c6929507933f11475fa4c8
Nicholas Griffin <me@nicholasgriffin.co.uk>
2022-10-22 21:55:27 +0100

    fix: changing url check to `startsWith`

--- src/utils/returnImageFormattingUrl.ts
+++ src/utils/returnImageFormattingUrl.ts
@@ -4,7 +4,7 @@ const ReturnImageFormattingUrl = (url) => {
     production: 'https://images.nicholasgriffin.dev',
   }[process.env.NODE_ENV];
 
-  if (url.includes('https://cdn.nicholasgriffin.dev/')) {
+  if (url.startsWith('https://cdn.nicholasgriffin.dev/')) {
     const noCDNURL = url.replace('https://cdn.nicholasgriffin.dev/', '');
 
     return `${baseUrl}/resize/?image=${noCDNURL}`;
